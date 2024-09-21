43add2a24937269a84231335dd10f33a53d683e9
IanD1013 <ian.tung1013@gmail.com>
2024-09-21 21:08:33 +1200

    Update Issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -32,7 +32,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setSubmitting(true);
-      await axios.post('/api/issues', data);
+      if (issue) await axios.patch(`/api/issues/${issue.id}`, data);
+      else await axios.post('/api/issues', data);
       router.push('/issues');
     } catch (error) {
       setSubmitting(false);
@@ -57,7 +58,9 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         <Controller defaultValue={issue?.description} name="description" control={control} render={({ field }) => <SimpleMDE placeholder="Description" {...field} />} />
         {<ErrorMessage>{errors.description?.message}</ErrorMessage>}
 
-        <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner />}</Button>
+        <Button disabled={isSubmitting}>
+          {issue ? 'Update Issue' : 'Submit New Issue'} {isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
